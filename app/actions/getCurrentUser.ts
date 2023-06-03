dbcc5dd7a330358c37c3867a968f7ca971fb4b83
skinet-web <skinet2009@gmail.com>
2023-06-03 22:47:36 +0300

    .

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -2,7 +2,7 @@ import { getServerSession } from "next-auth";
 
 
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import prisma from '../../libs/prismadb';
+import prisma from '../libs/prismadb';
 
 export async function getSession() {
     return await getServerSession(authOptions)
